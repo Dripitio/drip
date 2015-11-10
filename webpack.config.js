510c8d4836b07aeb679e4334e80af830f9e662ef
ktaube <t.kristaps@gmail.com>
2015-11-10 13:46:21 +0200

    Initial landingpage design

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.scss$/,
-      loaders: ["style", "css", "sass"]
+      loaders: ['style', 'css', 'sass']
     }]
   },
   resolve: {
