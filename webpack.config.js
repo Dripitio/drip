695ea7f6fc42e18561d1782fb599ef86ece34b26
ktaube <t.kristaps@gmail.com>
2015-11-10 20:27:12 +0200

    Use Babel loader for all jsx files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 var path = require('path'),
   webpack = require('webpack');
 
+var staticPath = './drip/static';
+
 module.exports = {
-  context: path.join(__dirname, './drip/static'),
+  context: path.join(__dirname, staticPath),
   entry: {
-    dashboard: './js/dashboard/index.js',
-    landingpage: './js/landingpage/index.js'
+    dashboard: './js/dashboard/index.jsx',
+    landingpage: './js/landingpage/index.jsx',
+    drip: './js/drip/drip.jsx'
   },
   module: {
-    loaders: [{
-      test: /\.scss$/,
-      loaders: ['style', 'css', 'sass']
-    }]
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
   },
   resolve: {
-    root: path.join(__dirname, './drip/static'),
-    extensions: ['', '.js', '.scss']
+    root: path.join(__dirname, staticPath),
+    extensions: ['', '.scss', '.js', '.jsx']
   },
   output: {
     filename: '[name].bundle.js'
