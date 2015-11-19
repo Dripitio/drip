d0662fe21702273906afba92b8b8cdea50effe2a
Normunds Vilcins <n.vilcins@gmail.com>
2015-11-19 17:13:57 +0200

    fixed campaign task with mongo and logger initialization + comments

--- drip/tasks/campaign_tasks.py
+++ drip/tasks/campaign_tasks.py
@@ -1,21 +1,34 @@
+import os
 from drip.backend.campaign_processor import process_campaigns
+from logging.handlers import RotatingFileHandler
+from drip.config import Config
+import mongoengine
+import logging
 
 
+# initiates a single drip campaign processor run
+# currently initializes mongo connection and logger independently
+# based on config (so that we can run this as cron job)
 def process_drip_campaigns_task():
-    from drip.config import Config
+    # get config
     conf = Config()
-    import mongoengine
+    # initialize mongo connection
     mongoengine.connect(
         conf.MONGODB_SETTINGS["db"],
         host=conf.MONGODB_SETTINGS["host"],
         port=conf.MONGODB_SETTINGS["port"])
-
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler(filename="nice.log")
-    logger = logging.getLogger(name="nice")
+    # get log directory and name and initialize logger
+    log_dir = conf.LOG_SETTINGS["log_dir"]
+    log_name = conf.LOG_SETTINGS["log_name"]
+    file_handler = RotatingFileHandler(
+        filename=os.path.join(log_dir, log_name + ".log"),
+        maxBytes=200000000,  # 200MB
+        backupCount=20,  # 20*200MB=4GB=nice
+    )
+    logger = logging.getLogger(name=log_name)
     logger.addHandler(file_handler)
     logger.setLevel(logging.INFO)
+    # run the processor
     process_campaigns(logger)
 
 
