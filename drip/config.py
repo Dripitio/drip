0a4262de2abe96ac1da0d5f92f08a95fb1afacde
Kristaps Taube <t.kristaps@gmail.com>
2016-02-22 10:42:14 +0000

    Remove old keys

--- drip/config.py
+++ drip/config.py
@@ -1,6 +1,5 @@
 class Config(object):
-    SHOPIFY_API_KEY = 'e90d00400dca5df8133b79166c51a4a5'
-    SHOPIFY_API_SECRET = 'eff99d809e13640a2db0bff592b771cd'
+    pass
 
 
 class Development(Config):
